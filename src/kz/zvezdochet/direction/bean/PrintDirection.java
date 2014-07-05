82abecac07f4761199e51e06ce28b9a8f4c6c201
nataly <info@zvezdochet.kz>
2014-07-05 21:09:06 +0600

    Переход на Eclipse 4

---
+++ src/kz/zvezdochet/direction/bean/PrintDirection.java
@@ -0,0 +1,48 @@
+package kz.zvezdochet.direction.bean;
+
+import kz.zvezdochet.bean.SkyPoint;
+
+/**
+ * Класс, предствляющий дирекцию для отчета
+ * @author nataly
+ *
+ */
+public class PrintDirection {
+	/**
+	 * Движущаяся небесная точка
+	 */
+	private SkyPoint skyPoint1;
+	/**
+	 * Статичная небесная точка
+	 */
+	private SkyPoint skyPoint2;
+	/**
+	 * Возраст дирекции
+	 */
+	private int age;
+	
+	public PrintDirection(SkyPoint p1, SkyPoint p2, int age) {
+		this.skyPoint1 = p1;
+		this.skyPoint2 = p2;
+		this.age = age;
+	}
+	
+	public SkyPoint getSkyPoint1() {
+		return skyPoint1;
+	}
+	public void setSkyPoint1(SkyPoint skyPoint1) {
+		this.skyPoint1 = skyPoint1;
+	}
+	public SkyPoint getSkyPoint2() {
+		return skyPoint2;
+	}
+	public void setSkyPoint2(SkyPoint skyPoint2) {
+		this.skyPoint2 = skyPoint2;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
