ce580a614d874be7c890140106578bed27a79421
Nataly Didenko <inbox@zvezdochet.guru>
2015-09-03 17:00:02 +0600

    Расчёт транзитов на день заменён на обычный расчёт на любой день без необходимости сохранения

--- src/kz/zvezdochet/direction/handler/DateCalcHandler.java
+++ src/kz/zvezdochet/direction/handler/DateCalcHandler.java
@@ -1,11 +1,8 @@
 package kz.zvezdochet.direction.handler;
 
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
-import java.util.GregorianCalendar;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import kz.zvezdochet.bean.Aspect;
 import kz.zvezdochet.bean.Event;
@@ -18,10 +15,8 @@ import kz.zvezdochet.core.handler.Handler;
 import kz.zvezdochet.core.service.DataAccessException;
 import kz.zvezdochet.core.ui.util.DialogUtil;
 import kz.zvezdochet.core.util.CalcUtil;
-import kz.zvezdochet.core.util.DateUtil;
 import kz.zvezdochet.direction.part.DatePart;
 import kz.zvezdochet.service.AspectService;
-import kz.zvezdochet.util.Configuration;
 
 import org.eclipse.e4.core.contexts.Active;
 import org.eclipse.e4.core.di.annotations.Execute;
@@ -35,85 +30,25 @@ import org.eclipse.e4.ui.model.application.ui.basic.MPart;
  * @author Nataly Didenko
  */
 public class DateCalcHandler extends Handler {
-	private List<SkyPointAspect> aged = null;
+	protected List<SkyPointAspect> aged = new ArrayList<SkyPointAspect>();
 
 	@Execute
 	public void execute(@Active MPart activePart) {
 		try {
-			aged = new ArrayList<SkyPointAspect>();
 			DatePart datePart = (DatePart)activePart.getObject();
 			if (!datePart.check(0)) return;
 			Event person = datePart.getPerson();
 
-			Configuration conf = person.getConfiguration();
-			List<Model> planets = conf.getPlanets();
-			List<Model> houses = conf.getHouses();
-			
 			updateStatus("Расчёт транзитов на указанную дату", false);
 			Date seldate = datePart.getDate();
+			Event event = new Event();
+			event.setBirth(seldate);
+			event.calc(false);
 
-			//подсчитываем количество дней от натальной даты до транзитной с учётом високосных годов
-			int year1 = person.getBirthYear();
-			Calendar calendar = GregorianCalendar.getInstance();
-			calendar.setTime(seldate);
-			int year2 = calendar.get(Calendar.YEAR);
-			boolean leap1 = false, leap2 = false;
-
-			long days = 0;
-			for (int y = year1; y <= year2; y++) {
-				if (DateUtil.isLeapYear(y)) {
-					days += 366;
-					if (y == year1)
-						leap1 = true;
-					if (y == year2)
-						leap2 = true;
-				} else
-					days += 365;
-			}
-
-			//вычитаем дни от 1 января натального года до натальной даты
-			long difdays = DateUtil.getDateDiff(person.getBirth(), DateUtil.getDate("01.01." + (year1 + 1)), TimeUnit.DAYS);
-			int ydays = leap1 ? 366 : 365;
-			days -= (ydays - difdays);
-			//вычитаем дни от транзитной даты до 1 января года, следующего за транзитным
-			difdays = DateUtil.getDateDiff(DateUtil.getDate("01.01." + year2), seldate, TimeUnit.DAYS);
-			ydays = leap2 ? 366 : 365;
-			days -= (ydays - difdays);
-
-			//
-//			double avey = ((366 * leaps) + (365 * (years - leaps)));
-			double age = days / 365.25;
-
-			//дирекции планеты к другим планетам
-			List<Model> trplanets = new ArrayList<Model>();
-			for (Model model: planets)
-				trplanets.add(new Planet((Planet)model));
-			for (Model model : trplanets) {
-				Planet trplanet = (Planet)model;
-				double coord = CalcUtil.getAgedCoord(Math.abs(trplanet.getCoord()), age);
-				trplanet.setCoord(coord);
-				for (Model model2 : planets) {
-					Planet planet = (Planet)model2;
-					calc(trplanet, planet, age);
-				}
-			}
-
-			//дирекции планеты к куспидам домов
-			for (Model model : trplanets) {
-				Planet trplanet = (Planet)model;
-				for (Model model2 : houses) {
-					House house = (House)model2;
-					calc(trplanet, house, age);
-				}
-			}
-			updateStatus("Расчёт транзитов завершён", false);
+			makeTransits(person, event);
 		    datePart.setData(aged);
 			updateStatus("Таблица транзитов сформирована", false);
 
-			Event event = new Event();
-			Configuration conf2 = new Configuration(seldate);
-			conf2.setPlanets(trplanets);
-			event.setConfiguration(conf2);
 			datePart.setEvent(event);
 			datePart.onCalc(0);
 			updateStatus("Космограмма транзитов сформирована", false);
@@ -128,10 +63,8 @@ public class DateCalcHandler extends Handler {
 	 * Определение аспектной дирекции между небесными точками
 	 * @param point1 первая небесная точка
 	 * @param point2 вторая небесная точка
-	 * @param age возраст
-	 * @param retro признак попятного движения
 	 */
-	private void calc(SkyPoint point1, SkyPoint point2, double age) {
+	private void calc(SkyPoint point1, SkyPoint point2) {
 		try {
 			//находим угол между точками космограммы с учетом возраста
 			double res = CalcUtil.getDifference(point1.getCoord(), point2.getCoord());
@@ -150,14 +83,40 @@ public class DateCalcHandler extends Handler {
 					aspect.setSkyPoint1(point1);
 					aspect.setSkyPoint2(point2);
 					aspect.setScore(res);
-					aspect.setAge(age);
 					aspect.setAspect(a);
 					aged.add(aspect);
 				}
 			}
 		} catch (Exception e) {
-			DialogUtil.alertError(point1.getNumber() + ", " + point2.getNumber() + ", " + age);
+			DialogUtil.alertError(point1.getNumber() + ", " + point2.getNumber());
 			e.printStackTrace();
 		}
+	}
+
+	/**
+	 * Расчёт транзитов
+	 * @param person персона
+	 * @param event событие
+	 */
+	protected void makeTransits(Event person, Event event) {
+		//дирекции планеты к другим планетам
+		List<Model> trplanets = event.getConfiguration().getPlanets();
+		for (Model model : trplanets) {
+			Planet trplanet = (Planet)model;
+			for (Model model2 : person.getConfiguration().getPlanets()) {
+				Planet planet = (Planet)model2;
+				calc(trplanet, planet);
+			}
+		}
+
+		//дирекции планеты к куспидам домов
+		for (Model model : trplanets) {
+			Planet trplanet = (Planet)model;
+			for (Model model2 : person.getConfiguration().getHouses()) {
+				House house = (House)model2;
+				calc(trplanet, house);
+			}
+		}
+		updateStatus("Расчёт транзитов завершён", false);
 	}
 }
--- src/kz/zvezdochet/direction/handler/TodayHandler.java
+++ src/kz/zvezdochet/direction/handler/TodayHandler.java
@@ -1,9 +1,10 @@
 package kz.zvezdochet.direction.handler;
 
-import kz.zvezdochet.core.handler.Handler;
+import java.util.Date;
+
+import kz.zvezdochet.bean.Event;
 import kz.zvezdochet.core.ui.util.DialogUtil;
-import kz.zvezdochet.direction.part.TransitPart;
-import kz.zvezdochet.part.ICalculable;
+import kz.zvezdochet.direction.part.DatePart;
 
 import org.eclipse.e4.core.contexts.Active;
 import org.eclipse.e4.core.di.annotations.Execute;
@@ -14,14 +15,28 @@ import org.eclipse.e4.ui.model.application.ui.basic.MPart;
  * @author nataly
  *
  */
-public class TodayHandler extends Handler {
+public class TodayHandler extends DateCalcHandler {
 	@Execute
 	public void execute(@Active MPart activePart) {
 		try {
 			updateStatus("Расчёт транзитов на сегодня", false);
-			ICalculable transitPart = (TransitPart)activePart.getObject();
-			transitPart.onCalc(0);
-			updateStatus("Карта транзитов на сегодня сформирована", false);
+			DatePart datePart = (DatePart)activePart.getObject();
+			if (!datePart.check(0)) return;
+			Event person = datePart.getPerson();
+
+			updateStatus("Расчёт транзитов на указанную дату", false);
+			Date seldate = datePart.getDate();
+			Event event = new Event();
+			event.setBirth(seldate);
+			event.calc(false);
+
+			makeTransits(person, event);
+		    datePart.setData(aged);
+			updateStatus("Таблица транзитов сформирована", false);
+
+			datePart.setEvent(event);
+			datePart.onCalc(0);
+			updateStatus("Космограмма транзитов сформирована", false);
 		} catch (Exception e) {
 			DialogUtil.alertError(e.getMessage());
 			updateStatus("Ошибка", true);
