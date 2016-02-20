fc26bab0ef62025ea779b19d9424e13f091a6773
Nataly Didenko <inbox@zvezdochet.guru>
2016-02-20 20:06:42 +0600

    Доработка экспорта событий

---
+++ src/kz/zvezdochet/direction/bean/DirectionAspectText.java
@@ -0,0 +1,19 @@
+package kz.zvezdochet.direction.bean;
+
+import kz.zvezdochet.analytics.bean.PlanetAspectText;
+import kz.zvezdochet.core.service.ModelService;
+import kz.zvezdochet.direction.service.DirectionAspectService;
+
+/**
+ * Толкование дирекции планеты в аспекте к другой планете
+ * @author Nataly Didenko
+ *
+ */
+public class DirectionAspectText extends PlanetAspectText {
+	private static final long serialVersionUID = -1376480936435498386L;
+
+	@Override
+	public ModelService getService() {
+		return new DirectionAspectService();
+	}
+}
