0eeab51061db83f53713787810dc52c770cea6e3
Natalie Didenko <inbox@zvezdochet.guru>
2020-04-15 19:40:51 +0600

    Адаптация под транзиты

--- src/kz/zvezdochet/direction/service/DirectionAspectService.java
+++ src/kz/zvezdochet/direction/service/DirectionAspectService.java
@@ -1,7 +1,11 @@
 package kz.zvezdochet.direction.service;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import kz.zvezdochet.analytics.service.PlanetAspectService;
 import kz.zvezdochet.core.bean.Model;
+import kz.zvezdochet.core.service.DataAccessException;
 import kz.zvezdochet.direction.bean.DirectionAspectText;
 
 /**
@@ -17,5 +21,14 @@ public class DirectionAspectService extends PlanetAspectService {
 	@Override
 	public Model create() {
 		return new DirectionAspectText();
+	}
+
+	@Override
+	public DirectionAspectText init(ResultSet rs, Model model) throws DataAccessException, SQLException {
+		DirectionAspectText dict = (model != null) ? (DirectionAspectText)model : (DirectionAspectText)create();
+		dict = (DirectionAspectText)super.init(rs, model);
+		dict.setDescription(rs.getString("description"));
+		dict.setCode(rs.getString("code"));
+		return dict;
 	}
 }
